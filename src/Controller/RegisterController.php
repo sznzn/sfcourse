75d36ca41a91655131ef971141d5a3b315dd1c52
sznzn <lu0045@hotmail.com>
2023-12-14 12:02:07 +0100

    bureau

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -1,18 +1,33 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
 
 class RegisterController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/register', name: 'register')]
-    public function register(Request $request)
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder)
     {
         $form = $this->createFormBuilder()
-            ->add('username', 'text')
+            ->add('username', TextType::class, ['label' => 'Username'])
             ->add('password', RepeatedType::class,[
                 'type' => PasswordType::class,
                 'invalid_message' => 'The password fields must match.',
@@ -21,12 +36,28 @@ class RegisterController extends AbstractController
                 'second_options' => ['label' => 'Repeat Password'],
             ])
             ->add('submit', SubmitType::class, [
-                'attr' => ['class' => 'btn btn-success float-end']
+                'attr' => ['class' => 'btn btn-success float-end mt-3']
             ])
-            -getForm()
+            ->getForm()
             ;
 
-            $form->handleRequest($request); // ici arreter 07.12
+            $form->handleRequest($request); 
+            if($form->isSubmitted()) {
+                $data = $form->getData();
+                $user = new User();
+                $user->setUsername($data['username']);
+                $user->setPassword(
+                    $passwordEncoder->encodePassword(
+                        $user,
+                        $data['password']
+                    )
+                );
+                dump($user);
+                die;
+                $user->setUsername;
+                $this->entityManager->persist($user);
+                $this->entityManager->flush();
+            }
         return $this->render('register/index.html.twig', [
             'form' => $form->createView()
         ]);
